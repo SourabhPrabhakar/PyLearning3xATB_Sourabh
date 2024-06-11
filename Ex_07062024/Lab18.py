e75622ee4c72be4e7c5a9e992eb26ec572a53ba9
Sourabh <sourabh.prabhakar11071993@gmail.com>
2024-06-11 22:46:37 +0530

    Adding files

---
+++ Ex_07062024/Lab18.py
@@ -0,0 +1,27 @@
+# Strings
+# Bunch of Char
+# '', "" , """
+name = 'Peter'
+print(name)
+name = "Peter"
+print(name)
+name = """Peter, Is is Good person
+He love to walk alone,
+....
+....
+....
+"""
+print(name)
+
+# Raw String
+dir = r'C:\nomedir\some dir'
+print(dir)
+
+# Format of the String
+first_name = "Peter"
+last_name = "Parker"
+print(first_name + " " + last_name)
+print(first_name, last_name)
+# f -> formatting - it will replace the values of the variables
+#{} -> placeholders
+print(f'Your Full name is {first_name} {last_name}')
