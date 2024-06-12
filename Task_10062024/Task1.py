f900bfea7cff4d41d293dc54b9616ab1fd31b995
Sourabh <sourabh.prabhakar11071993@gmail.com>
2024-06-12 07:14:27 +0530

    Adding files

--- Task_10062024/Task1.py
+++ Task_10062024/Task1.py
@@ -1,5 +1,17 @@
-# Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
-# import math
-number = int(input("Enter a number: "))
-print("Square of the number is: ", number ** 2, "\nCube of the number is: ", number ** 3)
-# print("Square of the number is: ", int(math.pow(number, 2)),"\nCube of the number is: ", int(math.pow(number,3)))
+# Task1:
+# Difference between = and == Operators in Python
+# = Operator: This is the assignment operator. It is used to assign a value to a variable.
+# For example, x = 5 assigns the value 5 to the variable x.
+
+# == Operator: This is the equality operator. It is used to compare two values to see if they are equal.
+# For example, x == 5 checks if the value of x is equal to 5.
+
+# The ** Operator in Python
+# The ** operator is the exponentiation operator. It is used to raise a number to the power of another number.
+# For example, 2 ** 3 calculates (2) raised to the power of (3), which equals (8).
+
+# The ^ Operator in Python
+# The ^ operator is the bitwise XOR (exclusive OR) operator. It is used to compare each bit of
+# two numbers and returns a new number whose bits are set to 1 where the corresponding bits of
+# either but not both operands are 1. For example, 5 ^ 3 results in 6 because the binary representation of
+# 5 is 101 and 3 is 011, and the XOR operation results in 110 which is 6 in decimal.
--- Task_10062024/Task2.py
+++ Task_10062024/Task2.py
@@ -1,3 +1,9 @@
+# Develop a Python script that calculates the square and cube of a given number. num = 2 sq - 4, c = 8
+# import math
+number = int(input("Enter a number: "))
+print("Square of the number is: ", number ** 2, "\nCube of the number is: ", number ** 3)
+# print("Square of the number is: ", int(math.pow(number, 2)),"\nCube of the number is: ", int(math.pow(number,3)))
+
 #Create a program that takes two numbers as input and prints whether
 # the first number is greater than, less than, or equal to the second number.
 num1 = float(input("Enter the first number: "))
